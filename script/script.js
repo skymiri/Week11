750c470a9e31cb3454bb80e8034ccb2f816847c2
skymiri <hnk0225@gmail.com>
2023-03-20 09:40:33 -0700

    gives two variables

--- script/script.js
+++ script/script.js
@@ -1,13 +1,27 @@
 //function declaration
-function greet(name) {
-  console.log(`hello ${name}`);
+function greet(time, name) {
+  console.log(`Good ${time} ${name}`);
 }
 //function calling
-greet("Nida");
+greet("Night", "Nida");
 
 // function expression
-const greet = function (name) {
+const greet1 = function (name) {
   console.log(`hello ${name}`);
 };
 
-greet("Nida");
+greet1("Nida");
+
+// arrow function
+let greet2 = (name) => {
+  console.log(`hello ${name}`);
+};
+
+greet2("Nida");
+
+function multiply(a, b = 1) {
+  return a + b;
+}
+
+console.log(multiply(5, 2));
+// expect 10
