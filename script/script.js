326f119faf3d4dff1f68043d6e7b0898eedfde2f
skymiri <hnk0225@gmail.com>
2023-03-20 09:25:57 -0700

    function decoration and declaration

--- script/script.js
+++ script/script.js
@@ -1,3 +1,13 @@
+//function declaration
 function greet(name) {
   console.log(`hello ${name}`);
 }
+//function calling
+greet("Nida");
+
+// function expression
+const greet = function (name) {
+  console.log(`hello ${name}`);
+};
+
+greet("Nida");
